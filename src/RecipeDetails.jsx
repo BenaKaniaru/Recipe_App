ec0eb186459577522b42d2c5d2e97814c683b28d
Benard Kaniaru <kaniarubenard623@gmailcom>
2025-03-28 15:06:37 +0300

    animated the recipe list and the recipe details

--- src/RecipeDetails.jsx
+++ src/RecipeDetails.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { TrophySpin, FourSquare } from "react-loading-indicators";
+import { motion, AnimatePresence } from "framer-motion";
 export default function RecipeDetails({ recipeId }) {
   const [loading, setLoading] = useState(false);
   const [detailsError, setDetailsError] = useState(null);
@@ -44,82 +45,93 @@ export default function RecipeDetails({ recipeId }) {
         </div>
       </div>
     );
-  if (loading) {
-    return (
-      <div className="fixed top-60 right-80">
-        <FourSquare color="#00008B" size="medium" text="loading..." />
-      </div>
-    );
-  }
 
   return (
-    <div className="flex-1 overflow-y-auto mb-4 px-8 shadow-lg bg-gray-100 rounded-lg shadow-black m-4">
-      {!recipeId && (
-        <div className="flex flex-col justify-center items-center gap-4 h-full">
-          <h1>No Recipe Selected Yet!</h1>
-          <div>
-            <TrophySpin
-              color={["red", "green", "blue", "orange"]}
-              size="large"
-              text="Select a Recipe to Display its Details Here!"
-              textColor="blue"
-            />
+    <AnimatePresence mode="await">
+      <motion.div
+        className="flex-1 overflow-y-auto mb-4 px-8 shadow-lg bg-gray-100 rounded-lg shadow-black m-4"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 20 }}
+        transition={{ duration: 0.5 }}
+      >
+        {!recipeId && (
+          <div className="flex flex-col justify-center items-center gap-4 h-full">
+            <h1>No Recipe Selected Yet!</h1>
+            <div>
+              <TrophySpin
+                color={["red", "green", "blue", "orange"]}
+                size="large"
+                text="Select a Recipe to Display its Details Here!"
+                textColor="blue"
+              />
+            </div>
           </div>
-        </div>
-      )}
+        )}
 
-      {recipeId && (
-        <div>
-          <h1 className="font-bold text-lg">
-            Recipe Details for {recipeDetails?.title}
-          </h1>
-          <div className="my-2 flex gap-1 md:gap-2 lg:gap-4 pt-4 italic text-sm md:text-md lg:text-lg font-semibold">
-            <span>⏳Ready in {recipeDetails?.readyInMinutes} Minutes</span>
-            <span>
-              {recipeDetails?.vegetarian ? "🥕Vegetarian" : "🍖Non-vegetarian"}
-            </span>
-            <span>
-              {recipeDetails?.glutenFree ? "Gluten Free" : "🌾Not Gluten Free"}
-            </span>
-            <span> Health Score: {recipeDetails?.healthScore}</span>
-          </div>
+        {recipeId && (
           <div>
-            <h1 className="font-black text-xl py-2">Ingredients</h1>
+            <h1 className="font-bold text-lg">
+              Recipe Details for {recipeDetails?.title}
+            </h1>
+            <div className="my-2 flex gap-1 md:gap-2 lg:gap-4 pt-4 italic text-sm md:text-md lg:text-lg font-semibold">
+              <span>⏳Ready in {recipeDetails?.readyInMinutes} Minutes</span>
+              <span>
+                {recipeDetails?.vegetarian
+                  ? "🥕Vegetarian"
+                  : "🍖Non-vegetarian"}
+              </span>
+              <span>
+                {recipeDetails?.glutenFree
+                  ? "Gluten Free"
+                  : "🌾Not Gluten Free"}
+              </span>
+              <span> Health Score: {recipeDetails?.healthScore}</span>
+            </div>
+            <div>
+              <h1 className="font-black text-xl py-2">Ingredients</h1>
 
-            {recipeDetails?.extendedIngredients?.map((ingredient) => (
-              <div
-                className="grid grid-cols-[1fr_2fr] shadow-xl p-4 bg-white rounded-lg text-center align-center mb-4 w-full"
-                key={ingredient.id}
-              >
-                <img
-                  className="w-100% h-auto rounded-lg mr-4"
-                  src={
-                    `https://spoonacular.com/cdn/ingredients_100x100/` +
-                    ingredient.image
-                  }
-                  alt=""
-                />
-                <div className="grid grid-cols-1 md:grid-cols-2 ml-2">
-                  <h3 className="font-semibold text-md text-left">
-                    {ingredient.name}
-                  </h3>
-                  <h3 className="font-semibold text-md text-left">
-                    {ingredient.amount} {ingredient.unit}
-                  </h3>
-                </div>
-              </div>
-            ))}
+              {recipeDetails?.extendedIngredients?.map((ingredient, index) => (
+                <motion.div
+                  className="grid grid-cols-[1fr_2fr] shadow-xl p-4 bg-white rounded-lg text-center align-center mb-4 w-full"
+                  key={ingredient.id}
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.8, delay: index * 0.5 }} // Stagger effect
+                >
+                  <img
+                    className="w-100% h-auto rounded-lg mr-4"
+                    src={
+                      `https://spoonacular.com/cdn/ingredients_100x100/` +
+                      ingredient.image
+                    }
+                    alt=""
+                  />
+                  <div className="grid grid-cols-1 md:grid-cols-2 ml-2">
+                    <h3 className="font-semibold text-md text-left">
+                      {ingredient.name}
+                    </h3>
+                    <h3 className="font-semibold text-md text-left">
+                      {ingredient.amount} {ingredient.unit}
+                    </h3>
+                  </div>
+                </motion.div>
+              ))}
+            </div>
+            <h1 className="font-black my-2 text-lg">
+              Preparation Instructions
+            </h1>
+            <ol className="list-decimal list-inside space-y-2 p-4 mb-2">
+              {recipeDetails?.analyzedInstructions[0]?.steps?.map(
+                (step, index) => (
+                  <li key={index}>{step.step}</li>
+                )
+              )}
+            </ol>
           </div>
-          <h1 className="font-black my-2 text-lg">Preparation Instructions</h1>
-          <ol className="list-decimal list-inside space-y-2 p-4 mb-2">
-            {recipeDetails?.analyzedInstructions[0]?.steps?.map(
-              (step, index) => (
-                <li key={index}>{step.step}</li>
-              )
-            )}
-          </ol>
-        </div>
-      )}
-    </div>
+        )}
+      </motion.div>
+    </AnimatePresence>
   );
 }
--- src/RecipeList.jsx
+++ src/RecipeList.jsx
@@ -1,4 +1,5 @@
 import { Riple } from "react-loading-indicators";
+import { motion, AnimatePresence } from "framer-motion";
 export default function RecipeList({
   recipeData,
   loading,
@@ -9,7 +10,9 @@ export default function RecipeList({
   return (
     <div
       className={
-        isMobile ? "w-full h-full" : "p-4 w-60 md:w-90 lg:w-120 overflow-y-auto"
+        isMobile
+          ? " w-full h-full text-sm"
+          : "p-4 w-60 md:w-90 lg:w-120 overflow-y-auto mx-auto"
       }
     >
       <div
@@ -33,33 +36,39 @@ export default function RecipeList({
             </div>
           )}
         </h1>
-        {recipeData && recipeData.length > 0
-          ? recipeData.map((recipe) => (
-              <div
-                key={recipe.id}
-                className={
-                  isMobile
-                    ? "bg-gray-200 p-4 w-full h-full rounded-lg flex flex-col gap-2 shadow-md shadow-black cursor-pointer hover:border-2 hover:border-gray-400"
-                    : "bg-gray-200 p-4 w-50 md:w-64 lg:w-96 rounded-lg flex flex-col gap-2 shadow-md shadow-black cursor-pointer hover:border-2 hover:border-gray-400"
-                }
-              >
-                <img
-                  className="rounded-lg"
-                  src={recipe.image ? recipe.image : "/download.jpeg"}
-                  alt="recipe image"
-                />
-                <h1 className="text-lg italic font-semi-bold">
-                  {recipe.title}
-                </h1>
-                <button
-                  className="bg-black p-1 w-32 text-white cursor-pointer hover:bg-gray-400"
-                  onClick={() => setRecipeId(recipe.id)}
+        <AnimatePresence mode="popLayout">
+          {recipeData && recipeData.length > 0
+            ? recipeData.map((recipe, index) => (
+                <motion.div
+                  key={recipe.id}
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: 20 }}
+                  transition={{ duration: 0.5, delay: index * 0.5 }}
+                  className={
+                    isMobile
+                      ? "bg-gray-200 p-4 w-full max-w-sm mx-auto h-full rounded-lg flex flex-col gap-2 shadow-md shadow-black cursor-pointer hover:border-2 hover:border-gray-400"
+                      : "bg-gray-200 p-4 w-50 md:w-64 lg:w-96 rounded-lg flex flex-col gap-2 shadow-md shadow-black cursor-pointer hover:border-2 hover:border-gray-400"
+                  }
                 >
-                  View Recipe
-                </button>
-              </div>
-            ))
-          : ""}
+                  <img
+                    className="rounded-lg w-3/4 m-auto"
+                    src={recipe.image ? recipe.image : "/download.jpeg"}
+                    alt="recipe image"
+                  />
+                  <h1 className="text-sm md:text-md lg:text-lg italic font-semi-bold">
+                    {recipe.title}
+                  </h1>
+                  <button
+                    className="bg-black p-1 w-32 text-white cursor-pointer hover:bg-gray-400"
+                    onClick={() => setRecipeId(recipe.id)}
+                  >
+                    View Recipe
+                  </button>
+                </motion.div>
+              ))
+            : ""}
+        </AnimatePresence>
       </div>
       <div className={loading ? "fixed top-75 left-40 z-10" : "hidden"}>
         <Riple color="#00008B" size="medium" text="loading" textColor="" />
